369e909f04a62af304ac82e5f5f54692a5d0c852
dan.ade <dan.ade@canstar.com.au>
2023-04-09 20:50:02 +1000

    modularise files
    rm type: module from package.json causing errors
    
    refine tsconfig.json to prevent ES errors

--- src/index.ts
+++ src/index.ts
@@ -3,53 +3,11 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { readFileSync } from 'fs';
 import mongoose, { Schema } from 'mongoose';
 import dotenv from 'dotenv';
+import resolvers from './graphql/resolvers';
 dotenv.config();
 
-const MONGODB = `mongodb+srv://danade:${process.env.MONGO_DB_PASSWORD}@cluster0.ejgeo.mongodb.net/?retryWrites=true&w=majority`;
+const MONGODB = `mongodb+srv://danade:${process.env.MONGO_DB_PASSWORD}@cluster0.ejgeo.mongodb.net/temporary?retryWrites=true&w=majority`;
 
-
-const recipeSchema = new Schema({
-  name: String,
-  description: String,
-  createdAt: String,
-  thumbsUp: Number,
-  thumbsDown: Number,
-});
-
-const Recipe = mongoose.model('Recipe', recipeSchema);
-
-export default Recipe;
-
-
-
-const resolvers = {
-  Query: {
-    async recipe(_: any, { ID }: any) {
-      return await Recipe.findById(ID);
-    },
-    async getRecipes(_, { amount }) {
-      return await Recipe.find().sort({ createdAt: -1 }).limit(amount);
-    },
-  },
-  Mutation: {
-    async createRecipe(_, { recipeInput: { name, description } }) {
-      const createdRecipe = new Recipe({
-        name: name,
-        description: description,
-        createdAt: new Date().toISOString(),
-        thumbsUp: 0,
-        thumbsDown: 0,
-      });
-
-      const res = await createdRecipe.save();
-      console.log(res);
-      return {
-        id: res.id,
-        // ...res._doc,
-      };
-    },
-  },
-};
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
